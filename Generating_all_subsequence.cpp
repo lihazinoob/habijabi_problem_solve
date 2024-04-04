3ca53b7c8297afd8ed64a7a51f1dbcacf317ea7e
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-04-04 14:59:52 +0600

    More easier  implementation, must needed in CP

--- Generating_all_subsequence.cpp
+++ Generating_all_subsequence.cpp
@@ -2,20 +2,26 @@
 using namespace std;
 vector<int>ans;
 int inputarr[100];
+bool is_taken[100];
 void generate_all_subsequence(int index,int n)
 {
-  if(index >= n)
+  if(index > n)
   {
-    for(auto i:ans)
+    for(int i = 1;i<=n;i++)
     {
-      cout<<i<<" ";
+      if(is_taken[i] == true)
+      {
+        cout<<inputarr[i]<<" ";
+      }
     }
     cout<<endl;
     return;
   }
-  ans.push_back(inputarr[index]);
+
+  is_taken[index] =false; //do not take
   generate_all_subsequence(index+1,n);
-  ans.pop_back();
+
+  is_taken[index] = true;
   generate_all_subsequence(index+1,n);
 
 }
@@ -23,11 +29,12 @@ int main()
 {
   int n;
   cin>>n;
-  for(int i = 0;i<n;i++)
+  for(int i = 1;i<=n;i++)
   {
     cin>>inputarr[i];
+    is_taken[i] = false;
   }
-  generate_all_subsequence(0,n);
+  generate_all_subsequence(1,n);
 
 
   return 0;
